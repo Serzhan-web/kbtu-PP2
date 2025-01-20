8152c83ee1d0717ee8a45b321248bb53ae440f7f
Serzhan-web <s_serikbaiuly@kbtu.kz>
2025-01-20 15:14:26 +0500

    Update Home.py

--- LABS/Lab_1/Home.py
+++ LABS/Lab_1/Home.py
@@ -1,1 +1,12 @@
+#Topics: HOME
 
+print("Hello, Almaty")
+
+#for output something to terminal
+#print("Type_string", Type_int)
+
+import sys
+
+print(sys.version)
+
+#To Check the Python version of the editor
