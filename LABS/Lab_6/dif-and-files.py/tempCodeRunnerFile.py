096f099bae44ddd80785f42558a3704861c6d064
Serzhan-web <s_serikbaiuly@kbtu.kz>
2025-03-02 23:12:10 +0500

    Внес некоторые изменения

--- LABS/Lab_6/dif-and-files.py/tempCodeRunnerFile.py
+++ LABS/Lab_6/dif-and-files.py/tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 C:/Users/Serzhan-web/Documents/kbtu-PP2/lab6/dif-and-files.py/9-exercise.py
+=======
+C:/Users/bekzatshaiyrgozha/Documents/PP2/lab6/dif-and-files.py/9-exercise.py
+>>>>>>> 6c22e5d (Внес некоторые изменения)
